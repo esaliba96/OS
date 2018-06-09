5d24bb73eafc4e270cbff0f12e9f923bba726bd1
Chameleon <ckenner@calpoly.edu>
2018-06-09 12:55:22 -0700

    test suite is working

--- P4/tinyFsDemo.c
+++ P4/tinyFsDemo.c
@@ -3,7 +3,11 @@
 int main(){
 	int fs;
 	tfs_mkfs("temp",10240);
+	printf("running test suite 1...\n\n");
 	testSuite1("temp");
+	printf("Waiting for 1 minute...\n\n");
+	sleep(60);
+	printf("running test suite 2...\n\n");
 	testSuite2("temp");
 	return 0;
 }
@@ -78,6 +82,7 @@ int testSuite2(char *fs_name){
 	tfs_seek(fd2, 243);
 	printf("Writing Character: Z\n\n");
 	tfs_writeByte(fd2, 'Z');
+	printInodeInfo("file2");
 	printf("Seeking to address: 89 in file3\n");
 	tfs_seek(fd3, 89);
 	printf("Writing Character: P\n\n");
@@ -86,13 +91,16 @@ int testSuite2(char *fs_name){
 	tfs_seek(fd3, 383);
 	printf("Writing Character: Q\n\n");
 	tfs_writeByte(fd3, 'Q');
+	printInodeInfo("file3");
 
 	printf("Making file2 Read-Only...\n\n");
 	tfs_makeRO("file2");
+	printInodeInfo("file2");
 	printf("Attempting to write to file2...\n");
 	tfs_writeByte(fd2, 'Q');
 	printf("\nMaking file2 Read/Write...\n\n");
 	tfs_makeRW("file2");
+	printInodeInfo("file2");
 
 	tfs_seek(fd2, 37);
 	tfs_readByte(fd2,list);
@@ -101,7 +109,7 @@ int testSuite2(char *fs_name){
 	tfs_seek(fd2, 243);
 	tfs_readByte(fd2,list);
 	printf("Reading Byte: %c from offset 243\n\n",list[0]);
-
+	printInodeInfo("file2");
 	tfs_seek(fd3, 89);
 	tfs_readByte(fd3,list);
 	printf("Reading Byte: %c from offset 89\n\n",list[0]);
@@ -109,8 +117,14 @@ int testSuite2(char *fs_name){
 	tfs_seek(fd3, 383);
 	tfs_readByte(fd3,list);
 	printf("Reading Byte: %c from offset 383\n\n",list[0]);
+	printInodeInfo("file3");
 
-	printf("Deleting file2\n\n");
+	printf("Renaming file2 to file4\n\n");
+	tfs_rename(fd2, "file4");
+	printInodeInfo("file4");
+	printSuperInfo();
+
+	printf("Deleting file4\n\n");
 	tfs_deleteFile(fd2);
 	printf("Deleting file3\n\n");
 	tfs_deleteFile(fd3);
@@ -134,8 +148,14 @@ void printSuperInfo(){
 void printInodeInfo(char* filename){
 	inodeblock inode;
 	readBlock(diskNO,hasFile(filename),&inode);
-	printf("\tFile : %s\n",filename);
+	printf("File Info for: %s\n",filename);
+	printf("\tFile Name : %s\n",filename);
 	printf("\tSize: %d\n",inode.file_size);
+	if(inode.RO == 1){
+		printf("\tPermissions: R\n");
+	} else{
+		printf("\tPermissions: RW\n");
+	}
 	struct tm tm = *localtime(&inode.cTime);
 	printf("\tcreated: %d-%d-%d %d:%d:%d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
 	struct tm tm1 = *localtime(&inode.aTime);
